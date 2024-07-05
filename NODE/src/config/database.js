2c7a0d735b66cf3d9579f612620ffb33b077bdd2
eliseu daniel <eliseudaniel29@gmail.com>
2024-07-04 22:50:35 -0300

    adm conclusion

--- NODE/src/config/database.js
+++ NODE/src/config/database.js
@@ -4,18 +4,31 @@ const {pool} = require('./config')
 
 dotenv.config()
 
-//apagar depois
-async function all(){
+async function allAdm(){
     const [sql] = await pool.execute('SELECT * FROM administrador')
     return sql
 }
 
-
-async function create(nome, senha, email, fone) {
-    const sql = 'INSERT INTO administrador (nomeAdm, senhaAdm, emailAdm, foneAdm) VALUES (?, ?, ?, ?)'
-    const {params} = await pool.execute(sql, [nome, senha, email, fone])
-    return {idAdm: params.insertidAdm, nome}
+async function showAdm(idAdm){
+    const [sql] = await pool.execute('SELECT * FROM administrador WHERE idAdm= ?', [idAdm])
+    return sql[0]
 }
 
+async function createAdm(nome, senha, email, fone) {
+    const sql = 'INSERT INTO administrador (nomeAdm, senhaAdm, emailAdm, foneAdm) VALUES (?, ?, ?, ?)'
+    const {params} = await pool.execute(sql, [nome, senha, email, fone])
+    return {id: params.insertid, nome}
+}
 
-module.exports = {all,create}
+async function updateAdm(idAdm, nome, senha, email, fone) {
+    const sql = 'UPDATE administrador SET nomeAdm= ?, senhaAdm= ?, emailAdm= ?, foneAdm= ? WHERE idAdm= ?'
+    const {params} = await pool.execute(sql, [nome, senha, email, fone, idAdm])
+    return {idAdm, nome, senha, email, fone}
+}
+
+async function deleteAdm(idAdm){
+    const [sql] = await pool.execute('DELETE FROM administrador WHERE idAdm= ?', [idAdm])
+    return sql[0]
+}
+
+module.exports = {showAdm, allAdm,createAdm, updateAdm, deleteAdm}
--- NODE/src/server.js
+++ NODE/src/server.js
@@ -5,10 +5,20 @@ const bd = require('./config/database')
 
 app.use(express.json())
 
-//apagar depois
+//Mostrar todos
 app.get('/adm', async (req,res,next) => {
     try{
-        const users = await bd.all()
+        const users = await bd.allAdm()
+        res.json(users)
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
+
+//Mostrar Adm
+app.get('/adm/:id', async (req,res,next) => {
+    try{
+        const users = await bd.showAdm(req.params.id)
         res.json(users)
     }catch(error){
         res.status(500).json({error: error.message})
@@ -19,15 +29,33 @@ app.get('/adm', async (req,res,next) => {
 app.post('/adm', async(req, res, next) => {
     try{
         const {nome, senha, email, fone} = req.body
-        const users = await bd.create(nome, senha, email, fone)
+        const users = await bd.createAdm(nome, senha, email, fone)
         res.json(users)
     }catch(error){
         res.status(500).json({error: error.message})
     }
 })
 
+//Atualizar Adm
+app.put('/adm/:id', async(req, res, next) => {
+    try {
+        const {nome, senha, email, fone} = req.body
+        const users = await bd.updateAdm(req.params.id, nome, senha, email,fone)
+        res.json(users)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
 
-
+//Deletar Adm
+app.delete('/adm/:id', async(req, res, next) => {
+    try {
+        const users = await bd.deleteAdm(req.params.id)
+        res.json(users)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
 
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}`)
