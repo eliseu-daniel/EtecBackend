b9c4f893e0932a0dce166555dd249e3b7ab3fe4a
eliseu daniel <eliseudaniel29@gmail.com>
2024-07-05 23:50:31 -0300

    starting clientes

--- NODE/src/server.js
+++ NODE/src/server.js
@@ -57,6 +57,61 @@ app.delete('/adm/:id', async(req, res, next) => {
     }
 })
 
+
+
+//Mostrar cliente
+app.get('/cliente', async (req,res,next) => {
+    try{
+        const users = await bd.allCli()
+        res.json(users)
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
+
+//Mostrar cliente
+app.get('/cliente/:id', async (req,res,next) => {
+    try{
+        const users = await bd.showCli(req.params.id)
+        res.json(users)
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
+
+//Criar cliente
+app.post('/cliente', async(req, res, next) => {
+    try{
+        const {nome, senha, email, fone} = req.body
+        const users = await bd.createCli(nome, senha, email, fone)
+        res.json(users)
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
+
+//Atualizar cliente
+app.put('/cliente/:id', async(req, res, next) => {
+    try {
+        const {nome, senha, email, fone} = req.body
+        const users = await bd.updateCli(req.params.id, nome, senha, email,fone)
+        res.json(users)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+//Deletar cliente
+app.delete('/cliente/:id', async(req, res, next) => {
+    try {
+        const users = await bd.deleteCli(req.params.id)
+        res.json(users)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}`)
 })
