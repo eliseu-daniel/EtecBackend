f0690d9deabd412b2dd148446938f83a72210dac
eliseu daniel <eliseudaniel29@gmail.com>
2024-09-02 22:35:04 -0300

    alterate response by /login

--- NODE/src/config/database.js
+++ NODE/src/config/database.js
@@ -1,18 +1,22 @@
 const mysql = require('mysql2/promise')
 const dotenv = require('dotenv')
-const {pool} = require('./config')
+const { pool } = require('./config')
 
 dotenv.config()
 
-async function getLogin(user, pw){
+async function getLogin(user, pw) {
+    if (!user || !pw) {
+        throw new Error('Parâmetros de login inválidos.');
+    }
+
     const sqlEmail = 'SELECT emailAdm FROM administrador WHERE emailAdm= ?'
     const [userEmail] = await pool.execute(sqlEmail, [user])
-    if(userEmail.length === 0){
+    if (userEmail.length === 0) {
         return "Usuário não cadastrado"
     }
     const sqlPw = 'SELECT emailAdm, senhaAdm FROM administrador WHERE emailAdm= ? AND senhaAdm= ?'
     const [Pw] = await pool.execute(sqlPw, [user, pw])
-    if(Pw.length === 0){
+    if (Pw.length === 0) {
         return "Senha Incorreta"
     }
 
@@ -20,125 +24,126 @@ async function getLogin(user, pw){
 }
 
 
-async function allAdm(){
+async function allAdm() {
     const [sql] = await pool.execute('SELECT * FROM administrador')
     return sql
 }
 
-async function showAdm(idAdm){
+async function showAdm(idAdm) {
     const [sql] = await pool.execute('SELECT * FROM administrador WHERE idAdm= ?', [idAdm])
     return sql[0]
 }
 
 async function createAdm(nome, senha, email, fone) {
     const sql = 'INSERT INTO administrador (nomeAdm, senhaAdm, emailAdm, foneAdm) VALUES (?, ?, ?, ?)'
-    const {params} = await pool.execute(sql, [nome, senha, email, fone])
-    return {nome, senha, email, fone}
+    const { params } = await pool.execute(sql, [nome, senha, email, fone])
+    return { nome, senha, email, fone }
 }
 
 async function updateAdm(idAdm, nome, senha, email, fone) {
     const sql = 'UPDATE administrador SET nomeAdm= ?, senhaAdm= ?, emailAdm= ?, foneAdm= ? WHERE idAdm= ?'
-    const {params} = await pool.execute(sql, [nome, senha, email, fone, idAdm])
-    return {idAdm, nome, senha, email, fone}
+    const { params } = await pool.execute(sql, [nome, senha, email, fone, idAdm])
+    return { idAdm, nome, senha, email, fone }
 }
 
-async function deleteAdm(idAdm){
+async function deleteAdm(idAdm) {
     const [sql] = await pool.execute('DELETE FROM administrador WHERE idAdm= ?', [idAdm])
     return idAdm
 }
 
 //Clientes
-async function allCli(){
+async function allCli() {
     const [sql] = await pool.execute('SELECT * FROM cliente')
     return sql
 }
 
-async function showCli(idCli){
+async function showCli(idCli) {
     const [sql] = await pool.execute('SELECT * FROM cliente WHERE idCliente= ?', [idCli])
     return sql[0]
 }
 
 async function createCli(nome, fone, email, senha) {
     const sql = 'INSERT INTO cliente (nomeCliente, foneCliente, emailCliente, senhaCliente) VALUES (?, ?, ?, ?)'
-    const {params} = await pool.execute(sql, [nome, fone, email, senha])
-    return {nome, fone, email, senha}
+    const { params } = await pool.execute(sql, [nome, fone, email, senha])
+    return { nome, fone, email, senha }
 }
 
 async function updateCli(idCli, nome, fone, email, senha) {
     const sql = 'UPDATE cliente SET nomeCliente= ?, foneCliente= ?, emailCliente= ?, senhaCliente= ? WHERE idCliente= ?'
-    const {params} = await pool.execute(sql, [nome, fone, email, senha, idCli])
-    return {idCli, nome, fone, email, senha}
+    const { params } = await pool.execute(sql, [nome, fone, email, senha, idCli])
+    return { idCli, nome, fone, email, senha }
 }
 
-async function deleteCli(idCli){
+async function deleteCli(idCli) {
     const [sql] = await pool.execute('DELETE FROM cliente WHERE idCliente= ?', [idCli])
     return idCli
 }
 
 //Pedidos
-async function allPed(){
+async function allPed() {
     const [sql] = await pool.execute(
         'SELECT pedido.idPedido, pedido.dataHora, pedido.statusPedido, cliente.nomeCliente FROM pedido INNER JOIN cliente ON pedido.idCliente = cliente.idCliente'
     )
     return sql
 }
 
-async function showPed(idPed){
+async function showPed(idPed) {
     const [sql] = await pool.execute('SELECT pedido.idPedido, pedido.dataHora, pedido.statusPedido, cliente.nomeCliente FROM pedido INNER JOIN cliente ON pedido.idCliente = cliente.idCliente WHERE idPedido= ?', [idPed])
     return sql[0]
 }
 
 async function createPed(dataHora, status, idCliente) {
     const sql = 'INSERT INTO pedido (dataHora, statusPedido, idCliente) VALUES (?, ?, ?)'
-    const {params} = await pool.execute(sql, [dataHora, status, idCliente])
-    return {dataHora, status, idCliente}
+    const { params } = await pool.execute(sql, [dataHora, status, idCliente])
+    return { dataHora, status, idCliente }
 }
 
 async function updatePed(idPed, dataHora, status, idCliente) {
     const sql = 'UPDATE pedido SET dataHora= ?, statusPedido= ?, idCliente= ? WHERE idPedido= ?'
-    const {params} = await pool.execute(sql, [dataHora, status, idCliente, idPed])
-    return {idPed, dataHora, status, idCliente}
+    const { params } = await pool.execute(sql, [dataHora, status, idCliente, idPed])
+    return { idPed, dataHora, status, idCliente }
 }
 
-async function deletePed(idPed){
+async function deletePed(idPed) {
     const [sql] = await pool.execute('DELETE FROM pedido WHERE idPedido= ?', [idPed])
     return idPed
 }
 
 
 //Item
-async function allItem(){
+async function allItem() {
     const [sql] = await pool.execute(
         'SELECT itempedido.idItem, itempedido.idPedido, itempedido.descricao, itempedido.quantidade, itempedido.precoUnitario, itempedido.nomeItem FROM itempedido INNER JOIN pedido ON itempedido.idPedido = pedido.idPedido'
     )
     return sql
 }
 
-async function showItem(idItem){
+async function showItem(idItem) {
     const [sql] = await pool.execute('SELECT itempedido.idItem, itempedido.idPedido, itempedido.descricao, itempedido.quantidade, itempedido.precoUnitario, itempedido.nomeItem FROM itempedido INNER JOIN pedido ON itempedido.idPedido = pedido.idPedido WHERE idItem= ?', [idItem])
     return sql[0]
 }
 
 async function createItem(idPedido, descricao, qtd, precoUn, nomeItem) {
     const sql = 'INSERT INTO itempedido (idPedido, descricao, quantidade, precoUnitario, nomeItem) VALUES (?, ?, ?, ?, ?)'
-    const {params} = await pool.execute(sql, [idPedido, descricao, qtd, precoUn, nomeItem])
-    return {idPedido, descricao, qtd, precoUn, nomeItem}
+    const { params } = await pool.execute(sql, [idPedido, descricao, qtd, precoUn, nomeItem])
+    return { idPedido, descricao, qtd, precoUn, nomeItem }
 }
 
-async function updateItem( idItem, idPedido, descricao, qtd, precoUn, nomeItem) {
+async function updateItem(idItem, idPedido, descricao, qtd, precoUn, nomeItem) {
     const sql = 'UPDATE itempedido SET idPedido= ?, descricao= ?, quantidade= ?, precoUnitario= ?, nomeItem= ? WHERE idItem= ?'
-    const {params} = await pool.execute(sql, [idPedido, descricao, qtd, precoUn, nomeItem, idItem])
-    return { idItem, idPedido, descricao, qtd, precoUn, nomeItem}
+    const { params } = await pool.execute(sql, [idPedido, descricao, qtd, precoUn, nomeItem, idItem])
+    return { idItem, idPedido, descricao, qtd, precoUn, nomeItem }
 }
 
-async function deleteItem(idItem){
+async function deleteItem(idItem) {
     const [sql] = await pool.execute('DELETE FROM itempedido WHERE idItem= ?', [idItem])
     return idItem
 }
 
-module.exports = {showAdm, allAdm, createAdm, updateAdm, deleteAdm,
-     showCli, allCli, createCli, updateCli, deleteCli,
+module.exports = {
+    showAdm, allAdm, createAdm, updateAdm, deleteAdm,
+    showCli, allCli, createCli, updateCli, deleteCli,
     getLogin,
     showPed, allPed, createPed, updatePed, deletePed,
     showItem, allItem, createItem, updateItem, deleteItem,
-    }
+}
--- NODE/src/server.js
+++ NODE/src/server.js
@@ -10,243 +10,249 @@ app.use(express.json())
 app.use(cors());
 
 //Login
-app.post('/login', async(req, res, next) => {
-    try{
-        const {email, senha} = req.body
+app.post('/login', async (req, res, next) => {
+    try {
+        const { email, senha } = req.body
+        console.log('Dados recebidos:', req.body);
         const users = await bd.getLogin(email, senha)
-        res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+        // res.json(users)
+        if (users === true) {
+            return res.status(200).json({ message: 'Login realizado com sucesso!' });
+        }
+        return res.status(400).json({ error: result })
+    } catch (error) {
+        console.error('Erro interno no servidor: ', error)
+        res.status(500).json({ error: 'Erro no servidor' })
     }
 })
 
 
 
 //Mostrar todos
-app.get('/adm', async (req,res,next) => {
-    try{
+app.get('/adm', async (req, res, next) => {
+    try {
         const users = await bd.allAdm()
         res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Mostrar Adm
-app.get('/adm/:id', async (req,res,next) => {
-    try{
+app.get('/adm/:id', async (req, res, next) => {
+    try {
         const users = await bd.showAdm(req.params.id)
         res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Criar Adm
-app.post('/adm', async(req, res, next) => {
-    try{
-        const {nome, senha, email, fone} = req.body
+app.post('/adm', async (req, res, next) => {
+    try {
+        const { nome, senha, email, fone } = req.body
         const users = await bd.createAdm(nome, senha, email, fone)
         res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Atualizar Adm
-app.put('/adm/:id', async(req, res, next) => {
+app.put('/adm/:id', async (req, res, next) => {
     try {
-        const {nome, senha, email, fone} = req.body
-        const users = await bd.updateAdm(req.params.id, nome, senha, email,fone)
+        const { nome, senha, email, fone } = req.body
+        const users = await bd.updateAdm(req.params.id, nome, senha, email, fone)
         res.json(users)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Deletar Adm
-app.delete('/adm/:id', async(req, res, next) => {
+app.delete('/adm/:id', async (req, res, next) => {
     try {
         const users = await bd.deleteAdm(req.params.id)
         res.json(users)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
 
 
 //Mostrar Todos cliente
-app.get('/cliente', async (req,res,next) => {
-    try{
+app.get('/cliente', async (req, res, next) => {
+    try {
         const users = await bd.allCli()
         res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Mostrar cliente
-app.get('/cliente/:id', async (req,res,next) => {
-    try{
+app.get('/cliente/:id', async (req, res, next) => {
+    try {
         const users = await bd.showCli(req.params.id)
         res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Criar cliente
-app.post('/cliente', async(req, res, next) => {
-    try{
-        const {nome, fone, email, senha} = req.body
+app.post('/cliente', async (req, res, next) => {
+    try {
+        const { nome, fone, email, senha } = req.body
         const users = await bd.createCli(nome, fone, email, senha)
         res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Atualizar cliente
-app.put('/cliente/:id', async(req, res, next) => {
+app.put('/cliente/:id', async (req, res, next) => {
     try {
-        const {nome, fone, email, senha} = req.body
+        const { nome, fone, email, senha } = req.body
         const users = await bd.updateCli(req.params.id, nome, fone, email, senha)
         res.json(users)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Deletar cliente
-app.delete('/cliente/:id', async(req, res, next) => {
+app.delete('/cliente/:id', async (req, res, next) => {
     try {
         const users = await bd.deleteCli(req.params.id)
         res.json(users)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
 
 
 //Mostrar Todos Pedidos
-app.get('/pedido', async (req,res,next) => {
-    try{
+app.get('/pedido', async (req, res, next) => {
+    try {
         const users = await bd.allPed()
         users.forEach(pedido => {
-            const dateIsoString = pedido.dataHora instanceof Date 
-            ? pedido.dataHora.toISOString() 
-            : pedido.dataHora
+            const dateIsoString = pedido.dataHora instanceof Date
+                ? pedido.dataHora.toISOString()
+                : pedido.dataHora
             pedido.dataHora = format(parseISO(dateIsoString), 'yyyy-MM-dd HH:mm:ss')
         })
         res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Mostrar Pedidos
-app.get('/pedido/:id', async (req,res,next) => {
-    try{
+app.get('/pedido/:id', async (req, res, next) => {
+    try {
         const users = await bd.showPed(req.params.id)
-        const dateIsoString = users.dataHora instanceof Date 
-            ? users.dataHora.toISOString() 
+        const dateIsoString = users.dataHora instanceof Date
+            ? users.dataHora.toISOString()
             : users.dataHora
         users.dataHora = format(parseISO(dateIsoString), 'yyyy-MM-dd HH:mm:ss')
         res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Criar Pedidos
-app.post('/pedido', async(req, res, next) => {
-    try{
-        const {dataHora, status, idCliente} = req.body
+app.post('/pedido', async (req, res, next) => {
+    try {
+        const { dataHora, status, idCliente } = req.body
         const parsedDate = parseISO(dataHora)
         const formattedDate = format(parsedDate, 'yyyy-MM-dd HH:mm:ss')
         const users = await bd.createPed(formattedDate, status, idCliente)
         res.json(users)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Atualizar Pedidos
-app.put('/pedido/:id', async(req, res, next) => {
+app.put('/pedido/:id', async (req, res, next) => {
     try {
-        const {dataHora, status, idCliente} = req.body
+        const { dataHora, status, idCliente } = req.body
         const parsedDate = parseISO(dataHora)
         const formattedDate = format(parsedDate, 'yyyy-MM-dd HH:mm:ss')
         const users = await bd.updatePed(req.params.id, formattedDate, status, idCliente)
         res.json(users)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Deletar Pedidos
-app.delete('/pedido/:id', async(req, res, next) => {
+app.delete('/pedido/:id', async (req, res, next) => {
     try {
         const users = await bd.deletePed(req.params.id)
         res.json(users)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
 
 
 //Mostrar Todos item
-app.get('/item', async (req,res,next) => {
-    try{
+app.get('/item', async (req, res, next) => {
+    try {
         const items = await bd.allItem()
         res.json(items)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Mostrar item
-app.get('/item/:id', async (req,res,next) => {
-    try{
+app.get('/item/:id', async (req, res, next) => {
+    try {
         const items = await bd.showItem(req.params.id)
         res.json(items)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Criar item
-app.post('/item', async(req, res, next) => {
-    try{
-        const {idPedido, descricao, qtd, precoUn, nomeItem} = req.body
+app.post('/item', async (req, res, next) => {
+    try {
+        const { idPedido, descricao, qtd, precoUn, nomeItem } = req.body
         const items = await bd.createItem(idPedido, descricao, qtd, precoUn, nomeItem)
         res.json(items)
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Atualizar item
-app.put('/item/:id', async(req, res, next) => {
+app.put('/item/:id', async (req, res, next) => {
     try {
-        const {idPedido, descricao, qtd, precoUn, nomeItem} = req.body
+        const { idPedido, descricao, qtd, precoUn, nomeItem } = req.body
         const items = await bd.updateItem(req.params.id, idPedido, descricao, qtd, precoUn, nomeItem)
         res.json(items)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
 //Deletar item
-app.delete('/item/:id', async(req, res, next) => {
+app.delete('/item/:id', async (req, res, next) => {
     try {
         const items = await bd.deleteItem(req.params.id)
         res.json(items)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
